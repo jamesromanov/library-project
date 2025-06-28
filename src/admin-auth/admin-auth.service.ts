52c58297d7d362115bec359eae0c3a6ebc31e0df
Jamshid Qahramonov <qahramonovjamshid1@gmail.com>
2025-06-28 11:10:40 +0500

    added swagger

--- src/admin-auth/admin-auth.service.ts
+++ src/admin-auth/admin-auth.service.ts
@@ -1,26 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAdminAuthDto } from './dto/create-admin-auth.dto';
-import { UpdateAdminAuthDto } from './dto/update-admin-auth.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AdminAuthService {
-  create(createAdminAuthDto: CreateAdminAuthDto) {
-    return 'This action adds a new adminAuth';
-  }
-
-  findAll() {
-    return `This action returns all adminAuth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} adminAuth`;
-  }
-
-  update(id: number, updateAdminAuthDto: UpdateAdminAuthDto) {
-    return `This action updates a #${id} adminAuth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} adminAuth`;
-  }
+  constructor(private readonly prisma: PrismaService) {}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.HEADER,
+    header: '1',
+  });
+  const config = new DocumentBuilder()
+    .setTitle("Tuproqqal'a TAKM api V1")
+    .setDescription("Tuproqqal'a TAKM Axborot kutubxona markazi api.")
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+  await app.listen(process.env.PORT ?? 5000, '0.0.0.0');
 }
 bootstrap();
