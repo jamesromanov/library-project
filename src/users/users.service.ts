8a5fefd895dfc78563a8e545fcdf89db1f2ac9c2
Jamshid Qahramonov <qahramonovjamshid1@gmail.com>
2025-07-05 18:24:14 +0500

    fixed access token err

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -96,8 +96,8 @@ export class UsersService {
     };
 
     const acceesToken = await this.jwt.sign(payload, {
-      secret: process.env.ACCESS_USER_TOKEN_KEY,
-      expiresIn: process.env.ACCESS_USER_TOKEN_EXP,
+      secret: process.env.ACCESS_TOKEN_KEY,
+      expiresIn: process.env.ACCESS_TOKEN_EXP,
     });
 
     return { acceesToken };
